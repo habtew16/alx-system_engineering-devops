fc6eb68a92cd2881270c92d73638a6446ef2bf09
habtew16 <132350715+habtew16@users.noreply.github.com>
2023-12-23 02:05:08 +0530

    Update 1-install_a_package.pp
    added extra installation requirments

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,19 @@
 #install a flask
+package {'python3.8':
+  ensure => '3.8.10'
+}
 
-package { 'flask':
+package {'python3-pip':
+  ensure => present,
+}
+
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => package['python3-pip'],
 }
