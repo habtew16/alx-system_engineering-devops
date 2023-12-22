e071992447cb94ee46a68e93b1800de9325b4899
habtew16 <132350715+habtew16@users.noreply.github.com>
2023-12-23 02:06:15 +0530

    Update 1-install_a_package.pp
    added comment

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,15 +3,18 @@ package {'python3.8':
   ensure => '3.8.10'
 }
 
+# install pip
 package {'python3-pip':
   ensure => present,
 }
 
+#install flask
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
+#install werkzug
 package {'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip',
