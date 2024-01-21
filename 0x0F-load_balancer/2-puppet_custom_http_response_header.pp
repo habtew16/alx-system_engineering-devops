71dc13fdb84cadc98c3e3ac124db1b05e53fb65a
habtew16 <habtamuwolde96@gmail.com>
2024-01-21 09:08:05 -0800

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # 2-puppet_custom_http_response_header.pp
 
 # Install Nginx package
@@ -6,11 +5,33 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-# Configure custom HTTP header in Nginx
-file { '/etc/nginx/sites-available/default':
+# Create index.html page
+file { '/var/www/html/index.html':
   ensure  => file,
-  replace => true,
-  content => template('nginx/default.erb'),
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Perform "moved permanently redirection" (301)
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => file,
+  content => template('default_redirect.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Create a 404 Custom error page
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => 'Ceci n\'est pas une page',
+  require => Package['nginx'],
+}
+
+# Add custom header X-Served-By
+exec { 'add_custom_header':
+  command => 'sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
+  path    => ['/bin', '/usr/bin'],
+  require => Package['nginx'],
   notify  => Service['nginx'],
 }
 
@@ -18,5 +39,6 @@ file { '/etc/nginx/sites-available/default':
 service { 'nginx':
   ensure    => 'running',
   enable    => true,
-  require   => File['/etc/nginx/sites-available/default'],
+  require   => File['/etc/nginx/sites-enabled/default'],
 }
+
