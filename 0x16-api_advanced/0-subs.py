dd4ac0194032965e6a7c3383eda7dbb3a5d3dbd0
= <=>
2024-02-16 08:01:50 -0800

    fixed something

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "MyRedditApp:1.0.0 (by /u/Fragrant_Role7558)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code >= 300:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    results = response.json().get("subscribers")
+    return results
